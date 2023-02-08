23fe1586ab261889c111bc7abe483e33e7298183
Harjot Singh <harjot.singh0712@gmail.com>
2023-02-08 12:56:04 -0800

    Applied cursorwarp patch

--- config.def.h
+++ config.def.h
@@ -8,6 +8,9 @@ static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
 
+/* cursor warping */
+static const bool cursor_warp = true;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
